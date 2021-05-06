70fac556922592c5434526aa6ea8f7866acd4611
devg1120 <devg1120@gmail.com>
2021-05-06 22:58:38 +0900

    first commit

---
+++ gs-app-spreahsheet/copy.sh
@@ -0,0 +1,4 @@
+rm -r ./node_modules/@syncfusion/ej2-angular-spreadsheet
+mkdir  ./node_modules/@syncfusion/ej2-angular-spreadsheet
+cp -r ~/Angular/angular-gs-component/gs-Component-lib/dist/spreadsheet/* ./node_modules/@syncfusion/ej2-angular-spreadsheet
+ls ./node_modules/@syncfusion/ej2-angular-spreadsheet
---
+++ gs-component-lib2/projects/admin/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { SpreadsheetAllModule } from '@syncfusion/ej2-angular-spreadsheet';
+//import  {SpreadsheetAllModule}  from '../../../spreadsheet/src/lib/spreadsheet-all.module';
+//import  {SpreadsheetAllModule}  from '../../../spreadsheet/src/public-api';
+
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+  ],
+  imports: [
+    BrowserModule ,
+    SpreadsheetAllModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
